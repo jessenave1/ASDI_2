cc2d8554cb12852a77d5f86b80b2700c6882d66b
Jesse Nave <jessenave@fullsail.edu>
2012-08-27 15:59:51 -0400

    first commit

---
+++ APL/APL/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  APL
+//
+//  Created by Jesse Nave on 8/27/12.
+//  Copyright (c) 2012 Jesse Nave. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ APL/APL/main.m
@@ -0,0 +1,24 @@
+//
+//  main.m
+//  APL
+//
+//  Created by Jesse Nave on 8/27/12.
+//  Copyright (c) 2012 Jesse Nave. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+    
+    float myFloat = 41;
+    
+    NSLog(@"myFloat%f", myFloat);
+    
+    
+}
