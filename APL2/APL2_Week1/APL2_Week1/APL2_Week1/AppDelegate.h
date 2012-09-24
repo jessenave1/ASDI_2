6df6467874471c27c45e32bd18c239b09fcaf9cd
Jesse Nave <jessenave@fullsail.edu>
2012-09-24 07:05:43 -0400

    first commit

---
+++ APL2/APL2_Week1/APL2_Week1/APL2_Week1/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  APL2_Week1
+//
+//  Created by Jesse Nave on 9/24/12.
+//  Copyright (c) 2012 Jesse Nave. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ APL2/APL2_Week1/APL2_Week1/APL2_Week1/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  APL2_Week1
+//
+//  Created by Jesse Nave on 9/24/12.
+//  Copyright (c) 2012 Jesse Nave. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
