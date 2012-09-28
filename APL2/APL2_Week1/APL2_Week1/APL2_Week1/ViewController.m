6e5ac767c99a77f04e6f3ee44416f18a71e9eec3
Jesse Nave <jessenave@fullsail.edu>
2012-09-28 07:32:49 -0400

    adding Buttons

--- APL2/APL2_Week1/APL2_Week1/APL2_Week1/ViewController.m
+++ APL2/APL2_Week1/APL2_Week1/APL2_Week1/ViewController.m
@@ -14,6 +14,11 @@
 
 @implementation ViewController
 
+const int button1Tag = 101;
+const int button2Tag = 102;
+const int button3Tag = 103;
+
+
 - (void)viewDidLoad
 {
     
@@ -57,7 +62,40 @@
         NSLog(@"%i", myAdobeBuildingTime);
     }
     
+    UIButton *logHouse = [UIButton buttonWithType:UIButtonTypeRoundedRect];
     
+    if (logHouse != nil) {
+        logHouse.tintColor = [UIColor whiteColor];
+        logHouse.frame = CGRectMake(10.0f, 250.0f, 100.0f, 50.0f);
+        logHouse.tag = button1Tag;
+        
+        [logHouse setTitle:@"Log House" forState:UIControlStateNormal];
+        [logHouse addTarget:self action:@selector(onClick:) forControlEvents:UIControlEventTouchUpInside];
+        [self.view addSubview:logHouse];
+    }
+    
+    UIButton *brickHouse = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    
+    if (brickHouse != nil) {
+        brickHouse.tintColor = [UIColor whiteColor];
+        brickHouse.frame = CGRectMake(10.0f, 250.0f, 100.0f, 50.0f);
+        brickHouse.tag = button2Tag;
+        
+        [brickHouse setTitle:@"Brick House" forState:UIControlStateNormal];
+        [brickHouse addTarget:self action:@selector(onClick:) forControlEvents:UIControlEventTouchUpInside];
+        [self.view addSubview:brickHouse];
+    }
+    UIButton *adobeHouse = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    
+    if (adobeHouse != nil) {
+        adobeHouse.tintColor = [UIColor whiteColor];
+        adobeHouse.frame = CGRectMake(10.0f, 250.0f, 100.0f, 50.0f);
+        adobeHouse.tag = button3Tag;
+        
+        [adobeHouse setTitle:@"Adobe House" forState:UIControlStateNormal];
+        [adobeHouse addTarget:self action:@selector(onClick:) forControlEvents:UIControlEventTouchUpInside];
+        [self.view addSubview:adobeHouse];
+    }
     
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
