17f87e170ed5316e3c7ed4932e6f70a0a8b5c9f4
Jesse Nave <jessenave@fullsail.edu>
2012-09-28 09:22:31 -0400

    working on ui

--- APL2/APL2_Week1/APL2_Week1/APL2_Week1/ViewController.m
+++ APL2/APL2_Week1/APL2_Week1/APL2_Week1/ViewController.m
@@ -21,7 +21,7 @@ const int button3Tag = 103;
 
 - (void)viewDidLoad
 {
-    //create loghouse and set log count
+    //create loghouse and set log count / instructions
     
     logHouse *myLogHouse = (logHouse *)[houseFactory createNewHouse:LOG];
     [myLogHouse setLogs:2500];
@@ -39,6 +39,8 @@ const int button3Tag = 103;
         [myLogHouse calculateBuildingTime];
     }
     
+    //create brick house and set brick type
+    
     brickHouse *myBrickHouse = (brickHouse*)[houseFactory createNewHouse:BRICK];
     
     if(myBrickHouse != nil)
@@ -52,6 +54,8 @@ const int button3Tag = 103;
         NSLog(@"%i", myHouseBuildingTime);
     }
     
+    //create adobe house and set square footage/instructions
+    
     adobeHouse *myAdobeHouse = (adobeHouse*)[houseFactory createNewHouse:ADOBE];
     if(myAdobeHouse != nil)
     {
@@ -63,6 +67,7 @@ const int button3Tag = 103;
         NSLog(@"%i", myAdobeBuildingTime);
     }
     
+    //UI Labels
     
     myLabel1 = [[UILabel alloc] initWithFrame:CGRectMake(0.0f, 10.0f, 100.0f, 20.0f)];
     if (myLabel1 != nil) {
@@ -71,25 +76,38 @@ const int button3Tag = 103;
         myLabel1.text = @"Log House: ";
         [self.view addSubview:myLabel1];
     }
-    myLabel2 = [[UILabel alloc] initWithFrame:CGRectMake(10.0f, 80.0f, 300.0f, 300.0f)];
+    myLabel2 = [[UILabel alloc] initWithFrame:CGRectMake(10.0f, 40.0f, 300.0f, 300.0f)];
     
     if (myLabel2 != nil) {
-        myLabel2.backgroundColor = [UIColor blueColor];
-        myLabel2.textColor = [UIColor redColor];
+        myLabel2.backgroundColor = [UIColor clearColor];
+        myLabel2.textColor = [UIColor whiteColor];
         myLabel2.textAlignment = UITextAlignmentLeft;
-        myLabel2.numberOfLines = 3;
         myLabel2.text = [NSString stringWithFormat:@"The %@ are used and you must %@.", [myLogHouse materials], [myLogHouse instructions]];
+        [myLabel2 setNumberOfLines:0];
+        [myLabel2 sizeToFit];
         [self.view addSubview:myLabel2];
     }
-    myLabel3 = [[UILabel alloc] initWithFrame:CGRectMake(0.0f, 300.0f, 320.0f, 60.0f)];
+    myLabel3 = [[UILabel alloc] initWithFrame:CGRectMake(0.0f, 160.0f, 120.0f, 20.0f)];
     if (myLabel3 != nil)
     {
-        myLabel3.backgroundColor = [UIColor lightGrayColor];
-        myLabel3.textColor = [UIColor blueColor];
-        myLabel3.text = @"Please enter Username";
-        myLabel3.textAlignment = UITextAlignmentCenter;
+        myLabel3.backgroundColor = [UIColor clearColor];
+        myLabel3.textColor = [UIColor blackColor];
+        myLabel3.text = @"Brick House: ";
         [self.view addSubview:myLabel3];
     }
+    
+    myLabel4 = [[UILabel alloc] initWithFrame:CGRectMake(0.0f, 180.0f, 300.0f, 300.0f)];
+    if(myLabel4 != nil)
+    {
+        myLabel4.backgroundColor = [UIColor redColor];
+        myLabel4.textColor = [UIColor blackColor];
+        //myLabel4.text = @"Hello world";
+        myLabel4.text = [NSString stringWithFormat:@"The house will use %i bricks, and require %d minutes to build.", [myBrickHouse brickType], [myBrickHouse buildingTimeMinutes]];
+        [myLabel4 setNumberOfLines:0];
+        [myLabel4 sizeToFit];
+        [self.view addSubview:myLabel4];
+    }
+    
     
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
