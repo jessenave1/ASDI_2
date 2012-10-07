2c54e6a4fcbf1ee1bcb6396a4a623892171be224
Jesse Nave <jessenave@fullsail.edu>
2012-10-06 20:40:30 -0400

    on/off switch

--- APL2/APL2_Week2/APL2_Week2/MainViewController.h
+++ APL2/APL2_Week2/APL2_Week2/MainViewController.h
@@ -10,12 +10,28 @@
 
 @interface MainViewController : UIViewController <FlipsideViewControllerDelegate>
 {
+    //Hooking up buttons and textfields
     IBOutlet UITextField *tf1;
+    IBOutlet UIButton *btn0;
+    IBOutlet UIButton *btn1;
+    IBOutlet UIButton *btn2;
+    IBOutlet UIButton *btn3;
+    IBOutlet UIButton *btn4;
+    IBOutlet UIButton *btn5;
+    IBOutlet UIButton *btn6;
+    IBOutlet UIButton *btn7;
+    IBOutlet UIButton *btn8;
+    IBOutlet UIButton *btn9;
+    IBOutlet UIButton *btn10;
+    IBOutlet UIButton *btn11;
+    IBOutlet UIButton *btn12;
+    //variables
     int firstNum;
     int secondNum;
     int answer;
 }
-
+//methods 
 - (IBAction)showInfo:(id)sender;
 - (IBAction)onClick:(id)sender;
+- (IBAction)onSwitched:(id)sender;
 @end
--- APL2/APL2_Week2/APL2_Week2/MainViewController.m
+++ APL2/APL2_Week2/APL2_Week2/MainViewController.m
@@ -17,9 +17,6 @@
 
 - (void)viewDidLoad
 {
-    int firstNum;
-    int secondNum;
-    int answer;
     
     [super viewDidLoad];
     
@@ -52,7 +49,7 @@
     controller.modalTransitionStyle = UIModalTransitionStyleFlipHorizontal;
     [self presentModalViewController:controller animated:YES];
 }
-
+//onClick actions for buttons
 - (IBAction)onClick:(id)sender
 {
     UIButton *btn = (UIButton*)sender;
@@ -100,14 +97,101 @@
         } else if(btn.tag == 10)
         {
             secondNum = firstNum;
+            tf1.text = @"";
             
         } else if(btn.tag == 11)
 {
     answer = (firstNum + secondNum);
     NSString *mystring = [NSString stringWithFormat:@"%d",answer];
     tf1.text = mystring;
+} else if (btn.tag == 12)
+{
+    tf1.text = @"";
+    firstNum = 0;
+    secondNum = 0;
+} else if(btn.tag == 13)
+{
+    
 }
 
+}
+}
+//add action to the on/off switch
+- (IBAction)onSwitched:(id)sender
+{
+    UISwitch *thisSwitch = (UISwitch*)sender;
+    if (thisSwitch != nil) {
+        
+        if (thisSwitch.isOn) {
+            tf1.enabled = true;
+        } else {
+            tf1.enabled = false;
+        }
+        if (thisSwitch.isOn) {
+            btn0.enabled = true;
+        } else {
+            btn0.enabled = false;
+        }
+        if (thisSwitch.isOn) {
+            btn1.enabled = true;
+        } else {
+            btn1.enabled = false;
+        }
+        if (thisSwitch.isOn) {
+            btn2.enabled = true;
+        } else {
+            btn2.enabled = false;
+        }
+        if (thisSwitch.isOn) {
+            btn3.enabled = true;
+        } else {
+            btn3.enabled = false;
+        }
+        if (thisSwitch.isOn) {
+            btn4.enabled = true;
+        } else {
+            btn4.enabled = false;
+        }
+        if (thisSwitch.isOn) {
+            btn5.enabled = true;
+        } else {
+            btn5.enabled = false;
+        }
+        if (thisSwitch.isOn) {
+            btn6.enabled = true;
+        } else {
+            btn6.enabled = false;
+        }
+        if (thisSwitch.isOn) {
+            btn7.enabled = true;
+        } else {
+            btn7.enabled = false;
+        }
+        if (thisSwitch.isOn) {
+            btn8.enabled = true;
+        } else {
+            btn8.enabled = false;
+        }
+        if (thisSwitch.isOn) {
+            btn9.enabled = true;
+        } else {
+            btn9.enabled = false;
+        }
+        if (thisSwitch.isOn) {
+            btn10.enabled = true;
+        } else {
+            btn10.enabled = false;
+        }
+        if (thisSwitch.isOn) {
+            btn11.enabled = true;
+        } else {
+            btn11.enabled = false;
+        }
+        if (thisSwitch.isOn) {
+            btn12.enabled = true;
+        } else {
+            btn12.enabled = false;
+        }
     }
 }
 @end
